bbdafcfb05770f693c9039d063616eb9db6bce8e
joachim <joachim@00101010.fr>
2022-02-26 08:16:56 +0100

    .

---
+++ srcs/parser/destroy_tree.c
@@ -0,0 +1,28 @@
+#include "minishell.h"
+typedef	void (*t_command_destructor)(union u_command c);
+
+//TODO
+
+void destroy_simple(union u_command c)
+{
+}
+void destroy_pipeline(union u_command c)
+{
+}
+void destroy_list(union u_command c)
+{
+}
+void destroy_grouping(union u_command c)
+{
+}
+void destroy_tree(t_command c)
+{
+	t_command_destructor a[] = {
+			&destroy_simple,
+			&destroy_pipeline,
+			&destroy_list,
+			&destroy_grouping
+	};
+
+	return(a[c.type](c.command));
+}
