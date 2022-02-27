9d637b3baaa5b8248d36e852ad7558b5d8d71cdc
00101010 <00101010@00101010.fr>
2022-02-27 18:40:41 +0100

    ...

--- srcs/utils/path_match.c
+++ srcs/utils/path_match.c
@@ -25,6 +25,8 @@ bool glob_include(char * glob, char * str)
 			++str;
 			++glob;
 		}
+		else
+			return (false);
 	} while (*glob && (*str || *glob == '*'));
 	return (*str == *glob);
 }
@@ -35,41 +37,52 @@ t_status path_match_recurse(char *path, char **array, t_str_vec *dst)
 	struct dirent	*entry;
 	char			*tmp;
 
-	current = opendir(path);
+	if (!*path)
+		current = opendir(".");
+	else
+		current = opendir(path);
 	if (!current)
 		return (KO);
 	entry = readdir(current);
 	while (entry)
 	{
-		if (glob_include(*array, entry->d_name))
+		if (ft_strcmp(entry->d_name, ".") && ft_strcmp(entry->d_name, "..") && glob_include(*array, entry->d_name))
 		{
-			path_push(path, *array);
-			if (is_dir(path))
+			path_push(path, entry->d_name);
+			if (is_dir(path) && array[1]) {
 				path_match_recurse(path, array + 1, dst);
-			else
+			}
+			else if (!array[1])
 			{
+				printf("# %s\n", path);
 				tmp = ft_strdup(path);
 				if (!tmp || !str_vec_push(dst, tmp))
+				{
+					closedir(current);
 					return (FATAL);
+				}
 			}
 			path_pop(path);
 		}
 		entry = readdir(current);
 	}
+	closedir(current);
+	return (OK);
 }
 
 char *path_match(char *str)
 {
-	char	**path;
-	char	tmp[PATH_MAX];
+	char	**array;
+	char	path[PATH_MAX];
 	t_str_vec	dst;
 
 	dst = str_vec_init();
-	path = ft_split(str, '/');
+	array = ft_split(str, '/');
 	// TODO: protect
 	if (*str == '/')
-		ft_strcpy(tmp, "/");
+		ft_strcpy(path, "/");
 	else
-		ft_strcpy(tmp, ".");
-
+		ft_strcpy(path, ".");
+	path_match_recurse(path,array, &dst);
+	return ("");
 }
