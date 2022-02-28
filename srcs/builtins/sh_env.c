9da6cce726cf172fdb11715e07fdc7507ae03323
Lara REALI <ereali@e2r9p15.clusters.42paris.fr>
2022-02-28 19:00:33 +0100

    env done

--- srcs/builtins/sh_env.c
+++ srcs/builtins/sh_env.c
@@ -8,13 +8,15 @@ unsigned char	sh_env(char **argv, t_env *env)
 	first = env->vars;
 	while (env->vars && env->vars->key)
 	{
-		ft_putstr_fd(env->vars->key, 1);
-		ft_putstr_fd("=", 1);
-		ft_putstr_fd(env->vars->value, 1);
-		ft_putstr_fd("\n", 1);
+		if (env->vars->is_exported == true)
+		{
+			ft_putstr_fd(env->vars->key, 1);
+			ft_putstr_fd("=", 1);
+			ft_putstr_fd(env->vars->value, 1);
+			ft_putstr_fd("\n", 1);
+		}
 		env->vars = env->vars->next;
 	}
 	env->vars = first;
-	printf("je suis de retour a first regarde %s\n", env->vars->key);
 	return (0);
 }
