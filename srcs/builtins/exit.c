5bb3cbc51383d8b2fef89ae4bc5935c0a31441e5
Lara REALI <ereali@e2r4p18.clusters.42paris.fr>
2022-03-07 18:42:02 +0100

    exit done

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: ereali <ereali@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 02:40:29 by ereali            #+#    #+#             */
-/*   Updated: 2022/03/04 20:53:51 by ereali           ###   ########.fr       */
+/*   Updated: 2022/03/07 18:36:37 by ereali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,18 @@ unsigned char	sh_exit(char **argv, t_env *env)
 	argv++;
 	if (!(*argv))
 		ft_exit(env);
+	if ((*argv)[i] == '-' && ft_isdigit((*argv)[i + 1]))
+		i++;
 	while (ft_isdigit((*argv)[i]))
 		i++;
 	if ((*argv)[i] == '\0'
 		&& !(nb_is_superior(*argv, "9223372036854775807")))
-		env->err = ft_atoi(*argv);
-	else if ((i >= 20 || nb_is_superior(*argv, "9223372036854775807"))
-		&& (*argv)[i] == '\0')
 	{
-		env->err = 2;
-		return (ft_putstr_fd("exit : numeric argument required\n", 2),
-			ft_exit(env), OK);
+		env->err = ft_atoi(*argv);
 	}
+	else if (!ft_isdigit((*argv)[i]) || ((i >= 20 || nb_is_superior(*argv,
+		"9223372036854775807")) && (*argv)[i] == '\0'))
+		return (ft_putstr_fd("exit : numeric argument required\n", 2), 2);
 	ft_exit(env);
 	return (0);
 }
